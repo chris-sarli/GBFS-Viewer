9a831c80b7ef15340bc5927c4321b4cc48b4c3f0
Chris Sarli <chris@Chris-MacBook-Pro.local>
2019-08-19 21:33:53 -0400

    Remove CORS workaround

--- main.js
+++ main.js
@@ -239,25 +239,6 @@ function checkIfLoaded() {
     }, 500);
 }
 
-
-
-(function () {
-    var cors_api_host = 'cors-anywhere.herokuapp.com';
-    var cors_api_url = 'https://' + cors_api_host + '/';
-    var slice = [].slice;
-    var origin = window.location.protocol + '//' + window.location.host;
-    var open = XMLHttpRequest.prototype.open;
-    XMLHttpRequest.prototype.open = function () {
-        var args = slice.call(arguments);
-        var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
-        if (targetOrigin && targetOrigin[0].toLowerCase() !== origin &&
-            targetOrigin[1] !== cors_api_host) {
-            args[1] = cors_api_url + args[1];
-        }
-        return open.apply(this, args);
-    };
-})();
-
 /**
  * Used to update the Leaflet layerControl with new layers.
  * @param {Integer} n The index of the layer in feedLayers to be added to the layerControl object.
