5ad836bd2e170e576e8de3f309df60e754a8ff07
Chris Sarli <chrismsarli@gmail.com>
2019-09-28 09:50:35 -0400

    Bye Lime

--- src/options.js
+++ src/options.js
@@ -88,26 +88,6 @@ let feedsToInclude = [
             }
         }
     },
-	{
-        url: "https://data.lime.bike/api/partners/v1/gbfs/providence/",
-        feed_name: "Lime",
-        freeVehicles: {
-            layerName: "Lime",
-            popup: true,
-            hideDefault: false,
-            displayOpts: {
-                type: "circle",
-                options: {
-                    radius: 5,
-                    color: "#FFFFFF",
-                    fillColor: "#45D700",
-                    fillOpacity: 0.9,
-                    opacity: 0.9,
-                    weight: 2
-                }
-            }
-        }
-    },
     {
         url: "https://web.spin.pm/api/gbfs/v1/providence/",
         feed_name: "Spin",
